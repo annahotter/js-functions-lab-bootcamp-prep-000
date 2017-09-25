3b95299219cda51e57bd54a7f620b4a7617ed31e
Anna Hotter <annajhotter@gmail.com>
2017-09-25 01:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 function happyHolidays() {return "Happy holidays!"}//
 function happyHolidaysTo(name) { return `Happy holidays, ${name}!` }
+function happyHolidayTo(holiday , name) { return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(days, holiday) {`It's ${days} days until ${holiday}!`}
